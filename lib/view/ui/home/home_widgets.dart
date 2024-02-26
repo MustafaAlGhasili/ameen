d49f482dbb98366441e1ff58cf7516fac7836b97
alhaddad-moha <mohammed@wosul.sa>
2024-02-27 00:52:09 +0300

    add google map

--- lib/view/ui/home/home_widgets.dart
+++ lib/view/ui/home/home_widgets.dart
@@ -1,11 +1,16 @@
+import 'dart:math';
+
 import 'package:ameen/controller/home_controller.dart';
 import 'package:ameen/view/ui/home/settings.dart';
 import 'package:ameen/view/ui/home/student_info.dart';
 import 'package:ameen/view/ui/widget/custom_divider.dart';
 import 'package:ameen/view/ui/widget/custom_state.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:iconly/iconly.dart';
+
 import '../widget/button_model.dart';
 
 HomeController controller = Get.find();
@@ -126,16 +131,69 @@ class HomePage extends StatelessWidget {
 }
 
 
+
+
+
+
+
+
+
 class Map extends StatelessWidget {
-  const Map({super.key});
+  const Map({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
+
+    // Define a single point on the map
+    LatLng point1 = LatLng(24.6583445, 46.6917818);
+    LatLng point2 = LatLng(24.8583445, 46.6917818);
+    ;
+
+    List<LatLng> polylinePoints = [point1, point2];
+
+
     return SizedBox(
       height: height * 0.7,
       width: width,
+      child: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: point1,
+          zoom: 15.0, // Set the desired zoom level for the first point
+        ),
+        myLocationEnabled: true,
+        markers: <Marker>{
+          Marker(
+            markerId: MarkerId('markerId1'),
+            position: point1,
+            infoWindow: InfoWindow(
+              title: 'Marker 1',
+              snippet: 'Point 1',
+            ),
+          ),
+          Marker(
+            markerId: const MarkerId('markerId2'),
+            position: point2,
+            infoWindow: InfoWindow(
+              title: 'Marker 2',
+              snippet: 'Point 2',
+            ),
+          ),
+        },
+/*
+        polylines: <Polyline>{
+          Polyline(
+            polylineId: PolylineId('polylineId'),
+            points: polylinePoints,
+            color: Colors.blue, // Set the color of the polyline
+            width: 5, // Set the width of the polyline
+          ),
+        },
+*/
+        // Padding is added to ensure the marker is not at the edges of the screen
+        padding: EdgeInsets.all(20.0),
+      ),
     );
   }
 }
