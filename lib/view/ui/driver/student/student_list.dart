febe42c5fa83580e20296b25b24605878b4840c3
Mustafa <mustakafa.733@gmail.com>
2024-03-26 15:14:34 +0300

    fix student_list.dart

--- lib/view/ui/driver/student/student_list.dart
+++ lib/view/ui/driver/student/student_list.dart
@@ -1,39 +1,25 @@
 import 'package:ameen/model/student.dart';
 import 'package:ameen/utils/constants.dart';
+import 'package:ameen/view/ui/driver/driver_home.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../../utils/DatabaseHelper.dart';
 import '../../admin/students/student_info.dart';
 
-class StudentList extends StatefulWidget {
+class StudentList extends StatelessWidget {
   const StudentList({super.key});
 
   @override
-  State<StudentList> createState() => _StudentListState();
-}
-
-class _StudentListState extends State<StudentList> {
-  DatabaseHelper dbHelper = DatabaseHelper();
-  Future<List<StudentModel>>? studentList;
-  List<StudentModel>? retrievedStudentList;
-
-  @override
-  void initState() {
-    super.initState();
-    _initRetrieval();
-  }
-
-  Future<void> _initRetrieval() async {
-    studentList = dbHelper.getStudentsByBusId('B1');
-    retrievedStudentList = await dbHelper.getStudentsByBusId('B1');
-  }
-  @override
   Widget build(BuildContext context) {
+    DatabaseHelper dbHelper = DatabaseHelper();
+
+    Future<List<StudentModel>>? studentList;
+
+    studentList = dbHelper.getStudentsByBusId('AA');
+
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-
-
 
     return Directionality(
       textDirection: TextDirection.rtl,
@@ -55,70 +41,61 @@ class _StudentListState extends State<StudentList> {
           child: FutureBuilder<List<StudentModel>?>(
             future: studentList,
             builder: (context, snapshot) {
-              print("slkndksv ${snapshot.data!}");
-
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    color: PRIMARY_COLOR,
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    "Error: ${snapshot.error}",
+                  ),
+                );
+              }
               return ListView.builder(
-                itemCount: retrievedStudentList!.length,
+                itemCount: snapshot.data!.length,
                 itemBuilder: (context, i) {
-
-
                   final student = snapshot.data;
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else if (snapshot.hasData) {
-                    return GestureDetector(
-                      onTap: () {
-                        Get.to(() => StudentDetails(
-                              student: student[i],
-                              no: 1,
-                            ));
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: PRIMARY_COLOR,
-                            borderRadius: BorderRadius.circular(20)),
-                        margin: EdgeInsets.symmetric(
-                            vertical: height * 0.015, horizontal: width * 0.05),
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(7.0),
-                              child: CircleAvatar(
-                                backgroundColor: Colors.white,
-                                backgroundImage: AssetImage(img[i]),
-                                radius:  width* 0.09,
-                              ),
+
+                  return GestureDetector(
+                    onTap: () {
+                      Get.to(() => StudentDetails(
+                            student: student[i],
+                            no: 1,
+                          ));
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: PRIMARY_COLOR,
+                          borderRadius: BorderRadius.circular(20)),
+                      margin: EdgeInsets.symmetric(
+                          vertical: height * 0.015, horizontal: width * 0.05),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(7.0),
+                            child: CircleAvatar(
+                              backgroundColor: Colors.white,
+                              backgroundImage: AssetImage(img[i]),
+                              radius: width * 0.09,
                             ),
-                            SizedBox(
-                              width: width * 0.015,
+                          ),
+                          SizedBox(
+                            width: width * 0.015,
+                          ),
+                          Text(
+                            student![i].fName,
+                            style: TextStyle(
+                              fontSize: width * 0.05,
+                              color: Colors.white,
                             ),
-                            Text(
-                              student![i].fName,
-                              style: TextStyle(
-                                fontSize: width * 0.05,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                    );
-                  } else if (snapshot.hasError) {
-                    return Center(
-                      child: Text(
-                        "${snapshot.error}",
-                        style: TextStyle(fontSize: width * 0.05),
-                      ),
-                    );
-                  }
-                  return Center(
-                    child: Text(
-                      "kkkkkk",
-                      style: TextStyle(fontSize: width * 0.05),
                     ),
-                  );;
+                  );
                 },
               );
             },
@@ -129,5 +106,5 @@ class _StudentListState extends State<StudentList> {
   }
 }
 
-// List<String> student = ["احمد خالد", "ساره عبد العزيز"];
+
 List<String> img = ['img/img.png', 'img/img2.png'];
